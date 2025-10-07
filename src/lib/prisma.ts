13863c4dac3b824042085bc6c259e6b35836712c
Silviojr13 <silviomarcaneiro@gmail.com>
2025-10-06 21:59:09 -0300

    ajuste para deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 
-const libsql = createClient({
+const adapter = new PrismaLibSQL({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
-});
+})
+const prisma = new PrismaClient({ adapter })
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const adapter = new PrismaLibSQL(libsql as any);
-
-export const prisma = new PrismaClient({ adapter });
 export default prisma;
